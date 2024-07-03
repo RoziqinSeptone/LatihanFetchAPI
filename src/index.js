4a2bc3614a3e4cab5f85052d18baf5878e1990dd
RoziqinSeptone <radjikinseptiawan@gmail.com>
2024-07-03 12:35:20 +0700

    membuat fitur filter search

--- src/index.js
+++ src/index.js
@@ -33,6 +33,49 @@ fetch('https://jsonplaceholder.typicode.com/users')
 
             </div>
         </div>`
-        });
+        })
         document.getElementById('containers').innerHTML = output
-      }) .catch(error => console.error('Gagal Mendapatkan Data',error))
+        
+        const searchInit = document.getElementById('searchInit')
+        searchInit.addEventListener('input',()=>{
+            const filtered  = data.filter(e => e.name.toLowerCase().includes(searchInit.value))
+            let filteredOutput = ""
+
+            filtered.forEach(user =>{
+                filteredOutput += `
+                <div >
+             
+              <div class="border">
+                  <h3 class="id">ID : ${user.id}</h3>
+                  <div class="card">                
+                      <h4>Name : ${user.name}</h4>
+                      <h4>Username : ${user.username}</h4>
+                      <p>Email : ${user.email}</p>
+                      <p>Phone :${user.phone}</p>
+                      <p>Address :
+                          <div class="address">
+                          <p>street :${user.address.street}</p>
+                          <p>suite : ${user.address.suite}</p>
+                          <p>city  : ${user.address.city}</p>
+                          <p>zipcode : ${user.address.zipcode}</p>
+                          </div>
+                          </p>
+                      <p>company :
+                          <div class="company">
+                              <p>bs : ${user.company.bs} </p>
+                              <p>CatchPhrase : ${user.company.catchPhrase}</p>
+                              <p>Name : ${user.company.name}</p>
+                          </div>
+                      </p>
+                      <p>Website : ${user.website}</p>
+              </div>
+  
+              </div>
+          </div>`
+            })
+
+         document.getElementById('containers').innerHTML = filteredOutput
+        
+        })
+    })
+        .catch(error => console.error('Gagal Mendapatkan Data',error))
